2142e335cf43e652c21634fa53e7017d076001a7
bje0716 <byon0716@gmail.com>
2014-02-28 07:46:07 +0900

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,33 @@
-add_lunch_combo buffer_e975-userdebug
+# LG (Phone)
+add_lunch_combo buffer_e975-userdebug # LG Optimus G (intl)
+add_lunch_combo buffer_e980-userdebug # LG Optimus G Pro (intl)
+add_lunch_combo buffer_d802-userdebug # LG G2 (intl)
+add_lunch_combo buffer_p930-userdebug # LG Optimus Nitro HD
+add_lunch_combo buffer_p990-userdebug # LG Optimus 2X
+
+# LG (Tablet)
+add_lunch_combo buffer_v500-userdebug # LG G Pad 8.3
+
+# Samsung
+add_lunch_combo buffer_galaxysmtd-userdebug # Samsung Galaxy S
+add_lunch_combo buffer_i9100-userdebug # Samsung Galaxy S2
+add_lunch_combo buffer_i9300-userdebug # Samsung Galaxy S3 3G (intl)
+add_lunch_combo buffer_n7000-userdebug # Samsung Galaxy Note
+add_lunch_combo buffer_n7100-userdebug # Samsung Galaxy Note 2 (GSM,Non-LTE)
+add_lunch_combo buffer_t0lte-userdebug # Samsung Galaxy Note 2 (GSM,LTE)
+add_lunch_combo buffer_hltexx-userdebug # Samsung Galaxy Note 3 (intl)
+add_lunch_combo buffer_dalikt-userdebug # Samsung Galaxy S2 HD LTE (KT)
+
+# Google (Phone)
+add_lunch_combo buffer_crespo-userdebug # Nexus S
+add_lunch_combo buffer_maguro-userdebug # Galaxy Nexus
+add_lunch_combo buffer_mako-userdebug # Nexus 4
+add_lunch_combo buffer_hammerhead-userdebug # Nexus 5
+
+# Google (Tablet)
+add_lunch_combo buffer_grouper-userdebug # Nexus 7
+add_lunch_combo buffer_flo-userdebug # Nexus 7 (2013)
+add_lunch_combo buffer_manta-userdebug # Nexus 10
+
+# Sony
+add_lunch_combo buffer_honami-userdebug # Sony Xperia Z1
